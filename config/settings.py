cc991df43921a2a17e516b26b7c7cd5066ca1d55
victormusembi11 <victor.musembi11@gmail.com>
2024-02-18 23:27:14 +0300

    feat: logout

--- config/settings.py
+++ config/settings.py
@@ -33,13 +33,9 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
-LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
 
 
 # Application definition
@@ -158,3 +154,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Authentication
+# https://docs.djangoproject.com/en/4.1/topics/auth/default/
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,12 @@ from django.urls import path, include
 
 urlpatterns = (
     [
+        path("admin/", admin.site.urls),
+        path("accounts/", include("django.contrib.auth.urls")),
         path("", include("core.urls")),
         path("items/", include("item.urls")),
         path("dashboard/", include("dashboard.urls")),
         path("inbox/", include("conversation.urls")),
-        path("admin/", admin.site.urls),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
